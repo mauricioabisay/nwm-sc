0b2c72b0853794b9d99f71331d2f8c820728151d
Mauricio Abisay <mauricioabisay.lopez@gmail.com>
2016-08-24 17:18:30 -0500

    Starting up

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+(function() {
+  var gulp    = require('gulp');
+  var bs      = require('browser-sync').create();
+  var plugins = require('gulp-load-plugins');
+  var $       = plugins();
+
+  gulp.task('jpg', function() {
+    gulp.src('./src/img/**/*.jpg')
+    .pipe($.imagemin({
+      pogressive: true
+    }))
+    .pipe(gulp.dest('./dest/img'));
+  });
+
+  gulp.task('png', function() {
+    gulp.src('./src/img/**/*.png')
+    .pipe($.imagemin({
+      optimizationLevel: 7
+    }))
+    .pipe(gulp.dest('./dest/img'));
+  });
+
+  gulp.task('gif', function() {
+    gulp.src('./src/img/**/*.gif')
+    .pipe($.imagemin({
+      optimizationLevel: 3
+    }))
+    .pipe(gulp.dest('./dest/img'));
+  });
+
+  gulp.task('svg', function() {
+    gulp.src('./src/img/**/*.svg')
+    .pipe($.imagemin())
+    .pipe(gulp.dest('./dest/img'));
+  });
+
+  gulp.task('sass', function() {
+    gulp.src('./src/styles/**/*')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('./dest'))
+    .pipe(bs.stream());
+  });
+
+  gulp.task('pug', function() {
+    gulp.src('./src/templates/**/*')
+    .pipe($.pug())
+    .pipe(gulp.dest('./dest'))
+    .pipe(bs.stream());
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch('./src/templates/**/*').on('change', bs.reload);
+    gulp.watch('./src/styles/**/*').on('change', bs.reload);
+  });
+
+  gulp.task('default', function() {
+    bs.init({
+      server: './dest'
+    });
+    gulp.watch('./src/templates/**/*', ['pug']).on('change', bs.reload);
+    gulp.watch('./src/styles/**/*', ['sass']).on('change', bs.reload);
+  });
+
+})();
